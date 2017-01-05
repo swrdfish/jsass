d52783ed14058bba721e5ac808acd1f484c43189
Binayak Ghosh <ghoshbinayak@gmail.com>
2017-01-05 21:05:38 +0530

    recursion done

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    var arr = [];
 
+    if (data.dir === dirName) {
+      return this.listFiles(data);
+    }
+
+    for (var pos = 0; pos < data.files.length; pos++){
+      if ( typeof data.files[pos] === 'object'){
+        arr = arr.concat(this.listFiles(data.files[pos], dirName));
+      }
+      else if (dirName === void 0){
+        arr.push(data.files[pos]);
+      }
+    }
+
+    return arr;
   },
 
   permute: function(arr) {
